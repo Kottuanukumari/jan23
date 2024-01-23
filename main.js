57e683182e6c66dc53ae05ebd99b2c28fb852e95
Kottuanukumari <154943471+Kottuanukumari@users.noreply.github.com>
2024-01-23 17:58:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var score=0;
+   
+function decrease(){
+    score=score-30;
+    document.getElementById("box").textContent=score;
+}
+function increase(){
+    score=score+30;
+    document.getElementById("box").textContent=score;
+}
